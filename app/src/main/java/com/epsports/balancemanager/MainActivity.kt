28e7df01e6a1a686cbad08067884351459521a67
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-26 03:21:52 +0600

    initial commit

--- app/src/main/java/com/epsports/balancemanager/MainActivity.kt
+++ app/src/main/java/com/epsports/balancemanager/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.epsports.balancemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
