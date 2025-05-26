9ee1f14455a66b4c2cf18b99e679bc9663b07ee4
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-27 01:11:43 +0600

    initial commit

---
+++ app/src/main/java/com/epsports/balancemanager/AnalysisFragment.kt
@@ -0,0 +1,16 @@
+package com.epsports.balancemanager
+
+import com.epsports.balancemanager.databinding.FragmentAnalysisBinding
+
+class AnalysisFragment : BaseFragment<FragmentAnalysisBinding>(FragmentAnalysisBinding::inflate) {
+
+
+    override fun setAllClickListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+}
