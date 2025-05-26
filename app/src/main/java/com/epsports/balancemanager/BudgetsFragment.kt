25452229120970604d5eb169a252befd0a3bfd74
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-27 01:11:59 +0600

    initial commit

---
+++ app/src/main/java/com/epsports/balancemanager/BudgetsFragment.kt
@@ -0,0 +1,15 @@
+package com.epsports.balancemanager
+
+import com.epsports.balancemanager.databinding.FragmentBudgetsBinding
+
+class BudgetsFragment : BaseFragment<FragmentBudgetsBinding>(FragmentBudgetsBinding::inflate) {
+    override fun setAllClickListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+
+}
